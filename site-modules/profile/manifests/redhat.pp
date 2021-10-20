b792011bc4538c3699904b5aeee499892e55be6d
Bill Glick <bglick@illinois.edu>
2021-10-20 13:36:10 -0500

    ASDPLNG-152: Add ncsa/rhsm module

--- site-modules/profile/manifests/redhat.pp
+++ site-modules/profile/manifests/redhat.pp
@@ -1,6 +1,14 @@
 # Place to include any modules that are needed only on RedHat systems
 class profile::redhat {
 
+  # Include some modules based off specific operatingsystem distro (RHEL vs CentOS etc)
+  case $facts['operatingsystem'] {
+    'RedHat' : {
+      include ::rhsm
+    }
+    default  : { } # do nothing
+  }
+
   include ::yum
 
 }
